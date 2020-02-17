b8748ee456ccc35ae1d57eb025933dffef55a2ad
Mischa Radenovic <miroslav.radenovic@gmail.com>
2020-02-16 21:37:10 -0500

    refactor: Extract DeckList container to a component

--- screens/DecksScreen.js
+++ screens/DecksScreen.js
@@ -1,33 +1,9 @@
 import * as React from 'react';
-import { connect } from 'react-redux';
-import { StyleSheet, Text, View } from 'react-native';
+import DeckList from '../containers/DeckList';
 
-function DecksScreen(props) {
-  const { decks } = props;
+export default function DecksScreen() {
 
   return (
-
-    <View style={styles.container}>
-      {decks.map( deck => (
-        <Text key={deck}>
-          Deck ID: {deck}
-        </Text>
-      ))}
-    </View>
+    <DeckList />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-  },
-});
-
-function mapStateToProps({decks}) {
-  return {
-    decks: Object.keys(decks)
-  };
-}
-
-export default connect(mapStateToProps)(DecksScreen);
