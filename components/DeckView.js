90f76d2a9f98ae2370e47132ec83d9684344e7f7
Mischa Radenovic <miroslav.radenovic@gmail.com>
2020-02-22 17:59:24 -0500

    refactor: Remopese deck view

--- components/DeckView.js
+++ components/DeckView.js
@@ -1,17 +1,16 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import colors from '../constants/Colors';
 import DeckViewActions from './DeckViewActions';
+import DeckInfo from './DeckInfo';
 
 function DeckView(props) {
   const { deck, showActions } = props;
-  const cardCount = Object.keys(deck.cards).length;
 
   return (
     <View style={styles.container}>
-      <Text>{deck.title}</Text>
-      <Text>{cardCount} card{cardCount !== 1 && 's'}</Text>
+      <DeckInfo deck={deck} />
       {showActions && <DeckViewActions id={deck.id} />}
     </View>
   );
