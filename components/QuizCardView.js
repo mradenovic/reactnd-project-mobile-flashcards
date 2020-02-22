209a20ac4aafbfbd9294cb355c397de698575882
Mischa Radenovic <miroslav.radenovic@gmail.com>
2020-02-22 17:06:14 -0500

    feat: Add flip card

--- components/QuizCardView.js
+++ components/QuizCardView.js
@@ -1,16 +1,47 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import FlipCard from 'react-native-flip';
 
 import QuizCardViewActions from './QuizCardViewActions';
 
 export default function QuizCardView(props) {
   const { card, handleResults } = props;
+  const [side, setSide] = React.useState(0);
 
   return (
-    <View>
-      <Text>{card.question}</Text>
-      <Text>{card.answer}</Text>
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.text}
+        onPress={() => {
+          setSide(side => (side === 0 ? 1 : 0));
+        }}
+      >
+        <FlipCard
+          style={styles.flipContainer}
+          side={side}
+          front={<Text style={styles.text}>Q: {card.question}</Text>}
+          back={<Text style={styles.text}>A: {card.answer}</Text>}
+        />
+      </TouchableOpacity>
       <QuizCardViewActions id={card.id} handleResults={handleResults} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+  flipContainer: {
+    margin: 10,
+    padding: 10,
+    height: 200,
+    width: 300,
+    borderRadius: 20,
+    borderWidth: 5,
+  },
+  text: {
+    alignSelf: 'center',
+    fontSize: 20
+  }
+});
