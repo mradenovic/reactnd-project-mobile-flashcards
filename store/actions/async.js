6dbc66308595ff76398c0cf7f77939f972b61a26
Mischa Radenovic <miroslav.radenovic@gmail.com>
2020-02-27 14:04:37 -0500

    feat: Store data to Async Storage

--- store/actions/async.js
+++ store/actions/async.js
@@ -1,25 +1,36 @@
 import { AsyncStorage } from 'react-native';
-import { deckActions } from '../index';
+import * as deckActions from '../reducers/decks';
 import example from '../example';
 
 const KEY = '@mobile-flashcards:decks';
 
-export const initDecks = () => async (dispatch) => {
+export const initDecks = () => async dispatch => {
   const data = await AsyncStorage.getItem(KEY);
-  const decks = data
-    ? JSON.parse(data)
-    : example;
+
+  if (!data) {
+    await AsyncStorage.setItem(KEY, JSON.stringify(example));
+  }
+
+  const decks = data ? JSON.parse(data) : example;
   dispatch(deckActions.initDecks(decks));
 };
 
-export const addDeck = ({ deck }) => (dispatch) => {
+export const addDeck = ({ deck }) => async dispatch => {
+  const newDeck = { [deck.id]: deck };
+  await AsyncStorage.mergeItem(KEY, JSON.stringify(newDeck));
   dispatch(deckActions.addDeck({ deck }));
 };
 
-export const addCard = ({ id, card }) => (dispatch) => {
+export const addCard = ({ id, card }) => async (dispatch, getState) => {
   dispatch(deckActions.addCard({ id, card }));
+  const deck = getState().decks[id];
+  const newDeck = { [deck.id]: deck };
+  await AsyncStorage.mergeItem(KEY, JSON.stringify(newDeck));
 };
 
-export const addScore = ({ id, score }) => (dispatch) => {
+export const addScore = ({ id, score }) => async (dispatch, getState) => {
   dispatch(deckActions.addScore({ id, score }));
+  const deck = getState().decks[id];
+  const newDeck = { [deck.id]: deck };
+  await AsyncStorage.mergeItem(KEY, JSON.stringify(newDeck));
 };
